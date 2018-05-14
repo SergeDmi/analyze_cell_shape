9ad5a5e035759e94331c2160a6cfdbd8d989fecc
SergeDmi <dmitrief@Serges-MacBook-Pro.local>
2018-05-14 09:08:34 +0200

    adding tools

---
+++ align_experiments.m
@@ -0,0 +1,23 @@
+function [ experiment ] = align_experiments( experiment )
+% We align the two cells 
+%   Nothing magical
+
+%% Loading and centering points
+pp1=experiment.pre_pombe.points;
+pp2=experiment.post_pombe.points;
+c1=mean(pp1);
+c2=mean(pp2);
+pp1=pp1-ones(size(pp1,1),1)*c1;
+pp2=pp2-ones(size(pp2,1),1)*c2;
+
+%% PCA to find the main axis of pre points
+[co1,p1]=princom(pp1);
+% Rotation of post according to PCA if 1
+p2=pp2*co1;
+
+%% Getting away with murder
+experiment.pre_pombe.points=p1;
+experiment.post_pombe.points=p2;
+
+end
+
