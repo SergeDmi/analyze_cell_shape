69b2cfb934ab12fbfb7ffb42ee6dbb36c3c9a520
SergeDmi <serge.dmitrieff@gmail.com>
2018-10-22 16:09:41 +0200

    significant code improvements, better skeleton, comments

--- analyze_pombe.m
+++ analyze_pombe.m
@@ -1,11 +1,14 @@
 function [ analysis,points] = analyze_pombe( pombe,options)
 %analyze_pombe analyzes the shape of pombe cells
-%   Analyzes a pombe segmentation
+%   Analyzes the shape of a pombe cell
 %   Nothing much for now
 %
 % Serge Dmitrieff, IJM 2018
 % www.biophysics.fr
 
+
+
+%% We  first test the input
 if nargin<2
     options=pombe_default_options();
     if isfield(options,'pixel_size')
@@ -15,14 +18,17 @@ if nargin<2
       pixel_size=1;
     end
 end
+% We resize the points if needed
+if options.do_pombe_resize
+  points=options.pixel_size*pombe.points;
+end
 
-points=options.pixel_size*pombe.points;
 normals=pombe.normals;
 faces=pombe.faces;
 
 
 %% Pre-treatment
-% Centering of the data...
+% Centering of the points...
 np=size(points,1);
 nf=size(faces,1);
 if options.centering > 0
@@ -32,7 +38,7 @@ if options.centering > 0
 		disp('Auto centering cell')
 	end
 end
-
+% Aligning the point
 if options.aligning > 0
 	 [~,points,~]=princom(points);
 	 if options.verbose>0
@@ -40,24 +46,24 @@ if options.aligning > 0
 	 end
 end
 
-
 % Now we're going to draw a central axis through the cell
-h=options.backbone.thickness/2.0;
+analysis.backbone_points=get_rolling_backbone(points,options.backbone);
+analysis.backbone_curv  =get_backbone_curvs(analysis.backbone_points);
+
+%% @DEPRECIATED
+% Old way to compute cell backbone
+
+%h=options.backbone.thickness/2.0;
 %pts_ix_center_z=logical((points(:,3)<h).*(points(:,3)>-h));
 %long_per=sort_by_angle(points(pts_ix_center_z,1:2),options);
+%pts_ix_center_z=logical((points(:,2)<h).*(points(:,2)>-h));
+%long_per=sort_by_angle(points(pts_ix_center_z,[1,3]),options);
+%[X_backbone,Y_backbone]=get_cell_backbone(long_per,options.backbone);
+%analysis.backbone_points=long_per;
+%analysis.backbone=spline_fit_backbone([X_backbone',Y_backbone'],options.backbone);
+%analysis.backbone_angular_deviation=compute_angular_deviations(analysis.backbone);
 
-pts_ix_center_z=logical((points(:,2)<h).*(points(:,2)>-h));
-long_per=sort_by_angle(points(pts_ix_center_z,[1,3]),options);
-[X_backbone,Y_backbone]=get_cell_backbone(long_per,options.backbone);
-analysis.backbone_points=long_per;
-analysis.backbone=spline_fit_backbone([X_backbone',Y_backbone'],options.backbone);
-analysis.backbone_angular_deviation=compute_angular_deviations(analysis.backbone);
-
-
-%np=size(points,1);
-%center=mean(points,1);
-%points=points-ones(np,1)*center;
-
+%% @DEPRECIATED : old way to align cells
 %% We should rotate points & normals
 % We use PCA to get the eigen vectors of the points
 %[coeffs]=princom(points);
@@ -73,58 +79,61 @@ analysis.backbone_angular_deviation=compute_angular_deviations(analysis.backbone
 
 %% We can already compute surface area.
 [analysis.surface,analysis.volume]=get_surface_volume(points,faces);
-
-
 [normsP,normsF,surfP,surfF,matR]=get_normals(points,faces,np,nf);
-[CP,CS]=compute_curvatures(points,faces,normsP,np,nf);
-analysis.surface_points=sum(surfP);
-analysis.surface_faces=sum(surfF);
 
-momsS=moments(CS,3);
-momsP=moments(CP,3);
-[ofs,ks,ths]=gamma_params(momsS);
-[ofp,kp,thp]=gamma_params(momsP);
-analysis.distrCS=[ofs,ks,ths];
-analysis.distrCP=[ofp,kp,thp];
-analysis.CS=CS;
-analysis.CP=CP;
-analysis.mean_curv=CP+CS;
-analysis.curv_energy=sum(exclude_extremes((analysis.mean_curv.^2).*surfP,3));
+%% We can compute the curvature of the cell wall on the main axes
+% CS corresponds to curvature on the cell main axis
+% CP corresponds to curvature on the cell short axis
+if (options.do_curvature_analysis)
+  [CP,CS]=compute_curvatures(points,faces,normsP,np,nf);
+  analysis.surface_points=sum(surfP);
+  analysis.surface_faces=sum(surfF);
 
+  momsS=moments(CS,3);
+  momsP=moments(CP,3);
+  [ofs,ks,ths]=gamma_params(momsS);
+  [ofp,kp,thp]=gamma_params(momsP);
+  analysis.distrCS=[ofs,ks,ths];
+  analysis.distrCP=[ofp,kp,thp];
+  analysis.CS=CS;
+  analysis.CP=CP;
+  analysis.mean_curv=CP+CS;
+  analysis.curv_energy=sum(exclude_extremes((analysis.mean_curv.^2).*surfP,3));
+end
 
-%% Now that it's turned, we can compute one or a couple thin slices
+%% We can also chop the cell into think slices and compute their shapes
 if (options.do_slice_analysis)
-  analysis.slices=compute_perimeters(points,options);
+  analysis.slices=make_analyze_slices(points,options);
   %h=options.thickness_central/2.0;
   ix=round(analysis.slices.nslice/2);
   latYZ=analysis.slices.latYZ(ix,:);
   sliceYZ=analysis.slices.per(ix).points(:,2:3);
 else
+  % We need at least the central slice
   h=options.thickness/2.0;
   gl=logical((points(:,1)<h).*(points(:,1)>-h));
   slice=points(gl,:);
   [~,sliceYZ,latYZ]=princom(slice(:,2:3));
 end
 
+%% We can approximate the centeral slice using a custom spline fitting method
 per=smooth_periodic(sliceYZ,options.central.spline);
 per=[per(:,end) per];
-analysis.central_perimeter=seglength(per);
-analysis.central_area=get_surface_slice(per);
-analysis.perimeter_angular_deviation=compute_angular_deviations(per(1:2,2:end)');
-analysis.central_spline=per;
-analysis.central_points=sliceYZ;
-% We do a PCA of the slice on the YZ plane
 
-%gl=logical((points(:,1)<h).*(points(:,1)>-h));
-%slice=points(gl,:);
-% We do a PCA of the slice on the YZ plane
-%[~,sliceYZ,latYZ]=princom(slice(:,2:3));
-% Random definition of circularity
-% This is quite sensitive cause we take variance of points
-% and then sqrt of ratio -> maximum sensitivity !
+
+%% From this we compute shapes
+[perimeter,area,excess_length]=compute_slice_shape(per);
+analysis.central_perimeter=perimeter;
+analysis.central_area=area;
+analysis.perimeter_angular_deviation=compute_angular_deviations(per(1:2,2:end)');
+analysis.central_spline=per';
+analysis.central_points=sliceYZ;
+% This is a bad way to compute circularity....
 analysis.central_circularity=1-sqrt(abs(latYZ(1)-latYZ(2))/sum(latYZ));
-%analysis.total_circularity=1-sqrt(abs(latXYZ(3)-latXYZ(3))/sum(latXYZ(2:3)));
-analysis.circularity_fiji=1-(sqrt(1-(4*pi*(analysis.central_area/(analysis.central_perimeter).^2))));
+% This is a better way
+analysis.circularity_fiji=1-(sqrt(1-(4*pi*(area/(perimeter^2)))));
+% Equivalently, we can compute the excess perimeter compared to a circle of similar area
+analysis.excess_length=excess_length;
 analysis.central_r1=sqrt(latYZ(1));
 analysis.central_r2=sqrt(latYZ(2));
 analysis.total_l1=max(points(:,1))-min(points(:,1));
@@ -132,8 +141,7 @@ analysis.pt_mean_dist=mean_dist(points,faces);
 analysis.length=max(points(:,1))-min(points(:,1));
 
 %@TODO : unified way to compute circularity and straightness...
-% Tough
-
+% Tough ! This is implemented in the compute_angular deviation part, works more or less
 
 
 %% Plotting if we need to
@@ -153,173 +161,239 @@ end
 
 end
 
+%% UTILITY FUNCTIONS
+
+% Sort a bunch of points by their angle
 function [PTS]=sort_by_angle(pts,options)
+  % Assumes PTS as a column vector of points (row of coordinates)
 np=size(pts,1);
 Pts=zeros(3,np);
 Pts(1:2,:)=pts';
+% converts to cylindrical coordinates to find the order
 cylpts=cart_to_cyl_z(Pts);
 [~,order]=sort(cylpts(1,:));
+% gotcha
 PTS=Pts(1:2,order)';
 end
 
+% This function computes the std deviation of the anges between consecutive segments in a set of points
 function [devi]=compute_angular_deviations(pts)
-  dp=normalize_rows_ND(diff(pts));
-  ns=size(dp,1);
-  projs=dp(1:ns-1,:).*dp(2:ns,:);
-  angs=acos(sum(projs,2));
-  devi=std(angs);
+% first find the orientation of segments
+dp=normalize_rows_ND(diff(pts));
+ns=size(dp,1);
+% we use the dot product to compute angle
+projs=dp(1:ns-1,:).*dp(2:ns,:);
+angs=acos(sum(projs,2));
+devi=std(angs);
 end
 
-function [PTS]=spline_fit_backbone(pts,options)
-  % Loading options
-  exclu=options.end_exclusion;
-  dt=options.spline.dt;
-  npp=options.spline.npp;
-  % getting the coordinates
-  x_max=max(pts(:,1));
-  x_min=min(pts(:,1));
-  ex_leng=exclu*(x_max-x_min);
-  x_max=x_max-ex_leng;
-  x_min=x_min+ex_leng;
-  new_x=(x_min:((x_max-x_min)*dt/(2.0*pi)):x_max);
-
-  np=size(pts,1);
-  n_new=numel(new_x);
-  PTS=zeros(n_new,2);
-
-  % Home-made spline fitting
-  dp=floor(np/npp);
-  npeff=ceil(np/npp);
-  indexes=1:np;
-  for i=1:npeff
-      ixes=indexes(i:dp:np);
-      PTS(:,2)=PTS(:,2)+spline(pts(ixes,1),pts(ixes,2),new_x)'/npeff;
-  end
-  PTS(:,1)=new_x';
-end
-
+% Normalize the rows in a column vector of row vectors
 function [M,w]=normalize_rows_3D(M)
 w=sqrt(sum(M.^2,2));
 M(:,:)=M./(w*[1 1 1]);
 end
 
+% We compute the shape of a slice of cell wall
+% The slice should be already splined !
+function [perimeter,area,excess_length]=compute_slice_shape(per)
+perimeter=seglength(per);
+area=get_surface_slice(per);
+% this is the relative excess perimeter of the slice compared to the circle of same surface area
+% with this definition, e_l gives dr/r for an ellipse of radii r,r+dr with dr -> 0
+excess_length=4*sqrt((perimeter^2)/(4*pi*area)-1);
+end
+
+% Computes the surface area of a smoothed slice
 function [surf]=get_surface_slice(per)
-    segs=diff(per,1,2);
-    %pp=per(:,1:end-1);
-    vecs=cross(segs,per(:,1:end-1),1);
-    surf=sum(sqrt(sum(vecs.^2,1)));
-    %surf=sum(vecs.^2)/2.0;
+% the surface area is the sum of the areas of the triangles between two consecutive points and the center
+% i.e. the sum of the cross products !
+segs=diff(per,1,2);
+vecs=cross(segs,per(:,1:end-1),1);
+surf=sum(sqrt(sum(vecs.^2,1)))/2.0;
+
 end
 
-function curvs=get_mean_curv(points,normsP,matR,np)
-curvs=zeros(np,1);
-coefs=zeros(np,1);
-for i=2:np
-	inters=logical(matR(i,1:(i-1)));
-	ni=sum(inters);
-	dist2s=sum((ones(ni,1)*points(i,:)-points(inters,:)).^2,2);
-	prods=(1-sum((ones(ni,1)*normsP(i,:)).*points(inters,:),2))./dist2s;
-	curvs(inters)=curvs(inters)+sqrt(prods);
-	coefs(inters)=coefs(inters)+1;
-	curvs(i)=curvs(i)+sum(prods);
-	coefs(i)=coefs(i)+1;
-end
-curvs=curvs./coefs;
-end
-
+% Compute the surface Jacobian
 function dNdAB=surface_grad(A,B,nA,nB)
-%invdir=1./(B-A);
-%dN=nB-nA;
 dNdAB=(1./(B-A)')*(nB-nA);
 end
 
-function [X,Y]=get_cell_backbone(long_per,options)
-n_pts=size(long_per,1);
-[xmin,n_xmin]=min(long_per(:,1));
-[xmax,n_xmax]=max(long_per(:,1));
-if n_xmin<n_xmax
-  if n_xmax==n_pts
-    ixes_r=(1:n_xmin);
-    ixes_l=((n_xmin+1):n_xmax);
-  else
-    ixes_r=[(n_xmax+1):n_pts (1:n_xmin)];
-    ixes_l=[(n_xmin+1):n_xmax];
-  end
-else
-  if n_xmin==n_pts
-    ixes_r=(1:n_xmax);
-    ixes_l=((n_xmax+1):n_xmin);
-  else
-    ixes_r=[(n_xmin+1):n_pts (1:n_xmax)];
-    ixes_l=[(n_xmax+1):n_xmin];
-  end
-end
-pts_l=long_per(ixes_l,:);
-pts_r=long_per(ixes_r,:);
-X=xmin:options.dx:xmax;
-y_left =spline(pts_l(:,1),pts_l(:,2),X);
-y_right=spline(pts_r(:,1),pts_r(:,2),X);
-Y=(y_left+y_right)/2.0;
+% computes the main curvatures of a discretized curve
+function [curvs]=get_backbone_curvs(ske)
+% we compute the arclength along the curve
+arclen=get_arclength(ske)';
+% we align the curve
+[~,ske]=princom(ske);
+% we compute the curvature of y,z as a function of the arclength
+py=polyfit(arclen,ske(:,2),2);
+pz=polyfit(arclen,ske(:,3),2);
+curvs=[py(1),pz(1)];
 end
 
+% Computes the backbone of a discretized shell using rolling average
+function [ske]=get_rolling_backbone(points,options)
+% If we really need, we can do it iteratively,
+% because sometimes x is a bad approximation for the arclength
+if isfield(options,'n_refine')
+  n_refine=options.n_refine;
+else
+  n_refine=1
+end
+
+% First we create segments
+n_pts=size(points,1);
+[xmin]=min(points(:,1));
+[xmax]=max(points(:,1));
+dx=options.dx;
+X=(xmin+dx/2.0):dx:(xmax-dx/2.0);
+nx=numel(X);
+ske=zeros(nx,3);
+ske(:,1)=X;
+
+% Now we (iteratively) refine the backbone
+for t=1:n_refine
+  [ske,dirs]=refine_backbone(ske,points,dx);
+end
+
+
+end
+
+%  computes arclength along a discretized curve
+function [lens]=get_arclength(M)
+  lens=get_cumulative_seglength(M);
+  lens=[0;lens]';
+end
+
+% computes the cumulative segment length of a discretized curve
+function [clen]=get_cumulative_seglength(M)
+  lens=get_seglenth(M);
+  clen=lens;
+  for i=2:numel(clen)
+    clen(i)=clen(i)+clen(i-1);
+  end
+end
+
+% computes the length of segments of a discretized curve
+function [lens]=get_seglenth(M)
+  sm=size(M);
+  segs=diff(M,1,1);
+  lens=sqrt(sum(segs.^2,2));
+end
+
+% Interpolates a curve regularly along its arclength
+function ske=interpolate_skeleton(ske)
+  s=get_arclength(ske);
+  S=linspace(0,max(s),numel(s));
+  if sum(s-S)~=0
+	  x=spline(s,ske(:,1),S);
+	  y=spline(s,ske(:,2),S);
+	  z=spline(s,ske(:,3),S);
+	  ske=[x',y',z'];
+  end
+end
+
+% compute oridentation at each point of a discretized curve.
+% extremities use a prior or posterior derivative
+% other points use a central derivative
+function [dirs]=get_directions(ske)
+  sk=size(ske);
+  nk=sk(1);
+  dirf=diff(ske);
+  dirs=zeros(sk);
+  if nk>2
+    dirs(2:(nk-1),:)=(dirf(1:(nk-2),:)+dirf(2:(nk-1),:))/2.0;
+    dirs(1,:)=dirf(1,:);
+    dirs(end,:)=dirf(end,:);
+  else
+    dirs(1,:)=dirf;
+    dirs(2,:)=dirf;
+  end
+  dirs=normalize_rows_3D(dirs);
+end
+
+% Takes a backbone ske, and makes it a closer backbone of the points points
+function [new_ske,new_dirs]=refine_backbone(ske,points,dx)
+sk=size(ske);
+sp=size(points);
+col1=ones(sp(1),1);
+row1=ones(1,3);
+% First we interpolate to have points regularly spaced along the arclength, without using the information on the points
+ske(:,:)=interpolate_skeleton(ske);
+dirs=get_directions(ske);
+% Now the skeleton is the rolling average of the points, around the previous backbone points
+for i=1:sk(1)
+  ptc=points-col1*ske(i,:);
+  dists=abs(dot(ptc,col1*dirs(i,:),2));
+  weights=exp(-(dists.^2)./(2.0*dx*dx));
+  new_ske(i,:)=sum(points.*(weights*row1),1 )./sum( weights,1);
+end
+new_dirs=get_directions(new_ske);
+end
+
+% Compute the normals to a suface
 function [normsP,normsF,surfP,surfF,matR]=get_normals(points,faces,np,nf)
-    % Marginal gain
+  % matR is an interaction matrix between points
 	matR=zeros(np,np);
+  % normsP are the normals to points
 	normsP=zeros(np,3);
+  % normsF are the normals to faces
 	normsF=zeros(np,3);
-	%surfF=zeros(nf,3);
-	%surfP=zeros(np,3);
+
+  % cntfP,F are the counts
 	cntfP=zeros(np,1);
 	cntfF=zeros(nf,1);
-    %A=zeros(1,3);
-    %B=zeros(1,3);
-    %C=zeros(1,3);
-    dir=zeros(1,3);
+  dir=zeros(1,3);
 	col3=ones(3,1);
 
 
-    for f=1:nf
+  for f=1:nf
+    % the director vector to a face is the cross products of the vectors
 		dir(:)=cross((points(faces(f,2),:)-points(faces(f,1),:)),(points(faces(f,3),:)-points(faces(f,1),:)));
 		matR(faces(f,1),faces(f,2))=1;
 		matR(faces(f,2),faces(f,3))=1;
 		matR(faces(f,3),faces(f,1))=1;
-        %surfP(faces(f,:),:)=surfP(faces(f,:),:)+
 		normsP(faces(f,:),:)=normsP(faces(f,:),:)+col3*dir;
 		cntfP(faces(f,:))=cntfP(faces(f,:))+1;
 		normsF(f,:)=dir;
 		cntfF(f)=cntfF(f)+1;
-
-
-		% Link A to B
-
-
-    end
-    [normsP(:,:),surfP]=normalize_rows_3D(normsP);
+  end
+  [normsP(:,:),surfP]=normalize_rows_3D(normsP);
 	[normsF(:,:),surfF]=normalize_rows_3D(normsF);
+
+  %surfP are the surfaces areas around each point
 	surfP(:)=surfP./cntfP;
+  %surfF are the surface areas of each face
 	surfF(:)=surfF./cntfF;
-
-
+  % well we basically get them for free :)
 end
 
+%  Here we do differential geometries
+% We compute two curvatures at each point
+% Cp is the curvature on the short axis
+% Cs is the curvature on the long axis (along the cell length)
 function [CP,CS]=compute_curvatures(points,faces,normsP,np,nf)
+% initiatlization
 poins(np).lCs=[];
-
 gradN=zeros(3,3);
 NP=zeros(3,1);
 NS=zeros(3,1);
-
 dirx=ones(np,1)*[1 0 0];
+
 % we create the basis
 n_psi=normalize_rows_3D(cross(normsP,dirx,2));
 n_sss=cross(n_psi,normsP,2);
 
+% We need to make a list of the local curvatures (shared with neighbours)
+% and their weight (the smaller the distance, the less precise we are)
+% because we'll have to do some dirty tricks to clean up from numerical errors
 for i=1:np
 	poins(i).lCs=[];
 	poins(i).lCp=[];
 	poins(i).wCs=[];
 	poins(i).wCp=[];
 end
+
+% for all faces, we will compute a bunch of curvatures !
 for f=1:nf
 	for i=1:3
 		if i==1
@@ -354,9 +428,11 @@ for f=1:nf
 	end
 end
 
+% The cleanup is here :)
 [CP,CS]=get_curvs(poins,np);
 end
 
+% We average the curvatures, while getting read of extreme values
 function [Cp,Cs]=get_curvs(poins,np)
 Cp=zeros(np,1);
 Cs=zeros(np,1);
@@ -375,9 +451,7 @@ for i=1:np
 end
 end
 
-
-
-
+% Using simple geometries we can quickly compute the volume and surface of a discretized space
 function [surf,vol]=get_surface_volume(points,face)
     nf=size(face,1);
     surf=0;
@@ -394,14 +468,16 @@ function [surf,vol]=get_surface_volume(points,face)
         C(:)=points(face(f,3),:);
         dir(:)=cross((B-A),(C-A));
         surf=surf+norm(dir);
+        % Here there is a small trick
         pos(:)=(A+B+C);
-        % This is so freacking cool !!!
         vol=vol+abs(dot(pos,dir));
+        % This is so freacking cool !!!
     end
     surf=surf/2;
     vol=vol/12.0;
 end
 
+% Computing the mean distance between connected points
 function [dist]=mean_dist(points,face)
     nf=size(face,1);
     np=0;
@@ -422,37 +498,43 @@ function [dist]=mean_dist(points,face)
     dist=dist/np;
 end
 
-
-function [res]=compute_perimeters(points,options)
+% we chop the poor cells in thin slices and we measure their shapes
+function [res]=make_analyze_slices(points,options)
+    % We prepare the slicing
     h=options.thickness_central/2.0;
     mp=min(points(:,1));
     Mp=max(points(:,1));
     N=floor((Mp-mp-2*h)/(2*h));
+
+    % we prepare the results
     res.pers=zeros(N,1);
     res.ares=zeros(N,1);
     res.pos=zeros(N,1);
-	res.dist=zeros(N,1);
+	  res.dist=zeros(N,1);
     res.latYZ=zeros(N,2);
     res.per(N).points=[0 0 0];
     xm=mp+2*h;
-    %figure
-    %hold all
+
+    % for all slices
     for i=1:N
-		pos=xm+(i*h*2)-h;
+        pos=xm+(i*h*2)-h;
         res.pos(i)=pos;
         res.dist(i)=min(abs(mp-pos),abs(Mp-pos));
 
         gl=logical((points(:,1)<(pos+h)).*(points(:,1)>(pos-h)));
         slice=points(gl,:);
+        % computing eigenvalues
         [~,~,res.latYZ(i,:)]=princom(slice(:,2:3));
-        %scatter3(slice(:,1),slice(:,2),slice(:,3))
+        % computing a smoother slice
         per= smooth_periodic( slice(:,2:3),options.central.spline);
 
-        %
+        % from that we get our sweet sweet results
         per=[per,per(:,1)];
+        [perim,ares,excl]=compute_slice_shape(per);
         res.circ(i)=1-sqrt(abs(res.latYZ(i,1)-res.latYZ(i,2))/sum(res.latYZ(i,:)));
-        res.ares(i)=get_surface_slice(per);
-        res.pers(i)=seglength(per);
+        res.ares(i)=ares;
+        res.pers(i)=perim;
+        res.excel(i)=excl;
         res.per(i).points=[res.pos(i)*ones(size(per,2),1) per(1:2,:)'];
     end
     res.nslice=N;
