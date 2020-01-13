0ad052b51cbfa15fd0f4f1043df895b1df11c9bf
SergeDmi <serge.dmitrieff@gmail.com>
2020-01-13 14:13:42 +0100

    updated pca

--- src/get_pca_cov.m
+++ src/get_pca_cov.m
@@ -4,6 +4,12 @@ function [rotmat, rotated, eigenvalues]=get_pca_cov(data,axis)
 %   axis is the axis of data repetition
 %  e.g. for a set of 3D points data , size(data)=3,N    , then axis=2 (default)
 %  e.g. for a set of 3D points data , size(data)=N,3    , then axis=1
+%
+% One should definitely use MATLAB's princom
+% But this way, no licence problem
+%
+% Serge Dmitrieff, 2020
+% www.biophysics.fr
 
 if nargin<2
   axis=2;
@@ -13,23 +19,24 @@ if axis==1
   data=data';
 end
 
-[n_rows,n_cols]=size(data);
+[~,n_cols]=size(data);
 
 % Centering data
 center=mean(data')';
 centered=data-repmat(center,1,n_cols);
 
 % Getting covariance matrix M
-M=(1 / (n_cols-1))*centered*centered';
+M=(1.0/(n_cols-1))*centered*centered';
 
 % We get the rotation matrix from the eigenvectors...
-[eigvectors,eigenvalues]=eig(M);
-rotmat=eigvectors';
+[eigenvectors,eigenvalues]=eig(M);
+rotmat=eigenvectors';
 rotated=rotmat*data;
 
 if axis==1
   rotated=rotated';
   rotmat=rotmat';
+  eigenvalues=eigenvalues';
 end
 
 end
